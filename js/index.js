c1fcc5cc2cae75a9105433d41a11c084b51ec1ee
1496-rajeev <1496piku@gmail.com>
2021-07-14 15:27:35 +0530

    console messages

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// console.clear();
+console.time("time took to load page")
+console.log("hello");
+console.table({ message: 'hello', number: 2 });
+console.warn("this is warning");
+console.timeEnd("time took to load page")
+console.error("this is an error")
+console.assert(5 > 20, "5>20 not possible")
