41e20d62be958c5146c7f3fa39299c1d75ffface
1496-rajeev <1496piku@gmail.com>
2021-07-21 00:28:24 +0530

    javascript prototype

--- js/index11.js
+++ js/index11.js
@@ -8,4 +8,44 @@ let obj = {
 };
 
 console.log(obj);
-figm;
+
+//constructor
+
+function Obj(givenName) {
+  this.name = givenName;
+}
+
+//prototype
+Obj.prototype.getName = function () {
+  return this.name;
+};
+
+//construct new object
+let obj2 = new Obj("rajeev");
+
+console.log(obj2);
+console.log(obj2.getName());
+
+//prototype inheritance
+const proto = {
+  slogan: function () {
+    return "this company is best";
+  },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+};
+
+//created an object1
+const rajeev = Object.create(proto);
+rajeev.name = "Rajeev";
+rajeev.role = "Programmer";
+rajeev.changeName("rajeev2");
+
+console.log(rajeev);
+
+//same as object1 only syntax change
+const rajeev1 = Object.create(pract, {
+  name: { value: "rajeev", writable: true },
+  role: { value: "programmer" },
+});
